f48d6c10b368d163c10e8b183721525cdf1017f0
Taslim <taslimahmed313@gmil.com>
2022-11-09 00:47:50 +0600

    Firebase SDK store in Environment Variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmPW5FNWQ1UXbAkJgfC5xQAJEkT9VL6oE",
-  authDomain: "attorna-law.firebaseapp.com",
-  projectId: "attorna-law",
-  storageBucket: "attorna-law.appspot.com",
-  messagingSenderId: "1047516286645",
-  appId: "1:1047516286645:web:944000cec2fb0d305af8fc",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
