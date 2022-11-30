38d6704e05339d88a9cb4e69a127e8c0778667e8
Taslim <taslimahmed313@gmil.com>
2022-11-30 12:17:33 +0600

    Home bar Edit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,18 +3,18 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-      <div className="banner lg:flex items-center justify-center p-4 text-white">
-        <div className="banner-section">
+      <div className="banner lg:flex lg:items-center lg:justify-center lg:py-0 pb-6 text-white">
+        <div className=" grid lg:grid-cols-2 items-center">
           <div>
             <img
-              className=" lg:w-[350px] h-[430px] banner-img"
+              className=" lg:w-[380px] w-full h-[500px] banner-img"
               src="https://img.freepik.com/free-photo/picture-elegant-young-fashion-man_158595-546.jpg?w=826&t=st=1668061907~exp=1668062507~hmac=893037c799311eff15e2226ba186a79b74833d800ee6cf667c169cef6baa7c93"
               alt=""
             />
           </div>
-          <div className="">
-            <p className=" lg:text-7xl font-mono font-semibold">We Enforce</p>
-            <h1 className=" text-6xl font-serif font-semibold">
+          <div className="lg:px-2 px-5 w-full">
+            <p className=" lg:text-7xl text-4xl font-mono font-semibold">We Enforce</p>
+            <h1 className=" lg:text-6xl text-3xl font-serif font-semibold">
               Justice For You
             </h1>
             <h3 className="lg:text-lg  uppercase text-center text-black font-medium bg-[#D9A86D] py-2 my-3 rounded-lg">
